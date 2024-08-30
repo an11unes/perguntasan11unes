d6456b420ba6775183b176950139a082d9a5e732
estudante alura <117747365+an11unes@users.noreply.github.com>
2024-08-30 17:17:39 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,143 @@
+export const perguntas = [
+    {
+        enunciado: "Qual você considera mais importante para o desempenho de um Porsche?",
+        alternativas: [
+            {
+                texto: "Desenvolver uma técnica de direção precisa.",
+                afirmacao: [
+                    "Você valoriza o domínio das técnicas de direção como essencial para o desempenho de um Porsche.",
+                    "Acredita que dirigir com precisão é fundamental para aproveitar ao máximo o potencial do carro."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Manter uma rotina de manutenção e cuidados regulares.",
+                afirmacao: [
+                    "Você acredita que a manutenção e os cuidados regulares são cruciais para o bom funcionamento do Porsche.",
+                    "Vê o desenvolvimento de uma rotina de manutenção como a base para garantir a performance do veículo."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você valoriza o aprendizado das técnicas de direção, qual destas opções você acha mais útil?",
+        alternativas: [
+            {
+                texto: "Participar de cursos de pilotagem especializados.",
+                afirmacao: [
+                    "Você acredita que cursos de pilotagem oferecem instruções detalhadas e aprimoram suas habilidades de direção.",
+                    "Vê a formação especializada como uma forma eficaz de aprender e aplicar técnicas avançadas de direção."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Assistir a vídeos e tutoriais sobre direção esportiva.",
+                afirmacao: [
+                    "Você valoriza o uso de vídeos e tutoriais como uma maneira prática de aprender novas técnicas de direção.",
+                    "Acredita que esses recursos oferecem uma forma acessível e flexível de aprimorar suas habilidades."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acha que vídeos e tutoriais sobre direção esportiva são mais úteis, como você vê a importância do treinamento presencial?",
+        alternativas: [
+            {
+                texto: "O treinamento presencial fornece feedback imediato e correção de técnicas.",
+                afirmacao: [
+                    "Você vê o treinamento presencial como uma forma importante de receber feedback direto e corrigir possíveis erros de direção.",
+                    "Acredita que a orientação ao vivo é fundamental para garantir a execução correta das técnicas de pilotagem."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "O treinamento presencial é menos relevante comparado à autonomia do aprendizado online.",
+                afirmacao: [
+                    "Você considera que a autonomia e o aprendizado autodidata têm mais impacto do que o treinamento presencial.",
+                    "Vê o aprendizado online como mais flexível e adaptável às suas necessidades pessoais."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acredita que o treinamento presencial é importante, como você avalia a importância de se manter atualizado com as novas tecnologias e tendências de veículos?",
+        alternativas: [
+            {
+                texto: "Manter-se atualizado é crucial para adaptar a condução e evitar a obsolescência.",
+                afirmacao: [
+                    "Você vê a atualização constante como uma necessidade para evitar a obsolescência e adaptar a condução às novas tecnologias.",
+                    "Acredita que estar informado sobre novas tendências é vital para continuar evoluindo como motorista."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Manter-se atualizado é secundário em comparação com a prática regular de direção.",
+                afirmacao: [
+                    "Você considera que a prática regular de direção é mais importante do que estar atualizado com as últimas tendências.",
+                    "Acredita que a consistência e o foco são mais decisivos para alcançar um bom desempenho do que as novas tecnologias."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você considera importante manter-se atualizado, qual formato você acha mais eficaz para aprender sobre novas tecnologias e tendências?",
+        alternativas: [
+            {
+                texto: "Participar de eventos e exposições automotivas.",
+                afirmacao: [
+                    "Você vê eventos e exposições automotivas como métodos eficazes para aprender diretamente sobre novas tecnologias e tendências.",
+                    "Acredita que esses eventos oferecem oportunidades práticas e de networking no setor automotivo."
+                ],
+            },
+            {
+                texto: "Leitura de artigos e revistas especializadas.",
+                afirmacao: [
+                    "Você valoriza a leitura de artigos e revistas como uma forma de se atualizar e aprofundar seu conhecimento de maneira autônoma.",
+                    "Acredita que esses recursos proporcionam uma visão detalhada e bem pesquisada sobre novas tecnologias e tendências."
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acha que a leitura de artigos é eficaz, como você vê a relação entre aprendizado autodidata e certificações automotivas?",
+        alternativas: [
+            {
+                texto: "O aprendizado autodidata pode ser tão eficaz quanto certificações automotivas, dependendo do contexto.",
+                afirmacao: [
+                    "Você considera que o aprendizado autodidata pode ser igualmente válido e útil, dependendo das circunstâncias e do contexto.",
+                    "Vê o autoestudo como uma abordagem flexível que pode complementar ou substituir certificações formais."
+                ],
+            },
+            {
+                texto: "Certificações automotivas são mais valiosas para garantir a qualidade e a validação do conhecimento adquirido.",
+                afirmacao: [
+                    "Você acredita que certificações formais oferecem um reconhecimento oficial e garantem a qualidade do conhecimento adquirido.",
+                    "Vê as certificações como uma forma importante de validar suas competências e habilidades no setor automotivo."
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acredita que certificações automotivas são mais valiosas, como você avalia a importância das habilidades interpessoais na interação com outros entusiastas e especialistas de veículos?",
+        alternativas: [
+            {
+                texto: "As habilidades interpessoais são essenciais para o sucesso e a integração com outros entusiastas e especialistas.",
+                afirmacao: [
+                    "Você considera que habilidades interpessoais são fundamentais para a eficácia e a dinâmica ao interagir com outros entusiastas e especialistas.",
+                    "Acredita que a capacidade de se comunicar e colaborar é tão importante quanto as qualificações técnicas."
+                ],
+            },
+            {
+                texto: "As habilidades interpessoais são importantes, mas as certificações e qualificações técnicas são mais decisivas para o sucesso.",
+                afirmacao: [
+                    "Você vê as certificações e qualificações técnicas como os principais fatores para o sucesso e a progressão no campo automotivo.",
+                    "Acredita que, embora as habilidades interpessoais sejam valiosas, elas não superam a importância das certificações formais."
+                ],
+            },
+        ]
+    }
+];
